ad184789cc49ab8006e951b7ed3c8e5f54a41ebc
municorn <municorn@musicaloft.com>
2021-08-01 02:31:26 -0600

    hm/sway: Use fish script for mpris-toggle

--- .config/nixpkgs/sway/files/mpris-toggle.sh
+++ .config/nixpkgs/sway/files/mpris-toggle.sh
@@ -1,61 +1,61 @@
-#!/bin/bash
+#!/bin/fish
 
-# original source: https://github.com/Icelk/dotfiles/blob/main/scripts/playerctl-custom.sh
+# original source: https://github.com/Icelk/dotfiles/blob/1fddc7c1f9120dab66e94273019909825cda8c73/scripts/playerctl-custom.sh
 
 # Clone of `playerctl`. Sends the second argument (e.g. `PlayPause` `Pause` etc.) to the first argument's media player.
-function pctl {
-    dbus-send --type=method_call --dest="org.mpris.MediaPlayer2.$1" /org/mpris/MediaPlayer2 "org.mpris.MediaPlayer2.Player.$2"
-}
+function pctl
+    dbus-send --type=method_call --dest="org.mpris.MediaPlayer2.$argv[1]" /org/mpris/MediaPlayer2 "org.mpris.MediaPlayer2.Player.$argv[2]"
+end
 
-# Super-fast compared to $(playerctl status)
+# Super-fast compared to (playerctl status)
 # Gets the property of the media player defined by the first argument filtered by the second argument.
 # To get the playback status of `spotifyd`, use `pctl_get "spotifyd" "PlaybackStatus"`
-function pctl_get {
+function pctl_get
     # Get the data from DBUS
-    response="$(dbus-send --reply-timeout=1000 --print-reply --dest="org.mpris.MediaPlayer2.$1" /org/mpris/MediaPlayer2 "org.freedesktop.DBus.Properties.Get" string:"org.mpris.MediaPlayer2.Player" string:"$2")"
+    set response (dbus-send --reply-timeout=1000 --print-reply --dest="org.mpris.MediaPlayer2.$argv[1]" /org/mpris/MediaPlayer2 "org.freedesktop.DBus.Properties.Get" string:"org.mpris.MediaPlayer2.Player" string:"$argv[2]")
     # If the response was a error
-    if [[ $? != 0 ]]; then
+    if test $status -ne 0
         echo "Stopped"
     else
         # Else get the actual response
-        echo "$(echo "$response" | tr ";" "\n")[2]"
-    fi
-}
+        echo (echo $response | string split '"')[2]
+    end
+end
 
 # Cache the initial status of spotifyd
-spt_status="$(pctl_get "spotifyd" "PlaybackStatus")"
+set spt_status (pctl_get "spotifyd" "PlaybackStatus")
 
 # Gets a media player (returned by `playerctl -l`). This prefers spotifyd above all other.
-function pref_spt {
-    player=$(playerctl -l | grep "spotifyd")
+function pref_spt
+    set player (playerctl -l | grep "spotifyd")
 
-    if [[ "$?" == 0 ]]; then
-        player=$(echo $player | head -n 1)
+    if test $status -eq 0
+        set player (echo $player | head -n 1)
     else
-        player=$(playerctl -l | head -n 1)
-    fi
+        set player (playerctl -l | head -n 1)
+    end
 
-    echo "$player"
-}
+    echo $player
+end
 
 # Loop on each media player
 # This checks for any media player playing, and if they are, pause and exit.
-for player in $(playerctl -l); do
+for player in (playerctl -l)
     # If it's spotifyd
-    if [[ "$player" == *"spotifyd"* ]]; then
+    if string match -q $player "spotifyd"
         # and playing, pause it.
-        if [[ "$spt_status" == *"Playing"* ]]; then
+        if string match -q $spt_status "Playing"
             pctl $player "Pause"
             exit 0
-        fi
+        end
     else
         # Else, if the status of the player is `Playing`, pause it and exit
-        if [[ "$(pctl_get $player "PlaybackStatus")" == *"Playing"* ]]; then
+        if string match -q (pctl_get $player "PlaybackStatus") "Playing"
             pctl $player "Pause"
             exit 0
-        fi
-    fi
-done
+        end
+    end
+end
 
-# If there was nothing to pause (we would've `exit`ed), play from the preferred player.
-pctl $(pref_spt) "Play"
+# If there was nothing to pause (we would've `exit`ed), play from the preferred editor.
+pctl (pref_spt) "Play"
