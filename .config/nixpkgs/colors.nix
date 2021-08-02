6b6b2400e881b3a1254079c0235db02cc54de948
municorn <municorn@musicaloft.com>
2021-08-01 19:24:09 -0600

    hm: Add solarized light theme

--- .config/nixpkgs/colors.nix
+++ .config/nixpkgs/colors.nix
@@ -77,6 +77,43 @@
     };
   };
 
+  # solarized light
+  solarizedLight = rec {
+    base00 = "fdf6e3";
+    base01 = "eee8d5";
+    base02 = "93a1a1";
+    base03 = "839496";
+    base04 = "657b83";
+    base05 = "586e75";
+    base06 = "073642";
+    base07 = "002b36";
+    base08 = "dc322f";
+    base09 = "cb4b16";
+    base0A = "b58900";
+    base0B = "859900";
+    base0C = "2aa198";
+    base0D = "268bd2";
+    base0E = "6c71c4";
+    base0F = "d33682";
+
+    palette = {
+      # background bases
+      background = base00;
+      black = base00;
+      gray = base01;
+
+      # foreground bases
+      foreground = base06;
+      white = base06;
+      silver = base05;
+
+      # other bases
+      primary = base0C;
+      warning = "ffaa00";
+      alert = "ff0000";
+    };
+  };
+
   eevee = rec {
     base00 = "2a1e12";
     base01 = "5b4128";
