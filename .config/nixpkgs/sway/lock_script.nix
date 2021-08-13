846038dde1d1d058e600b15a733597733e0edba6
municorn <municorn@musicaloft.com>
2021-08-12 18:51:45 -0600

    hm/sway: Modify lock script
    convert to fish and make it better

--- .config/nixpkgs/sway/lock_script.nix
+++ .config/nixpkgs/sway/lock_script.nix
@@ -1,57 +1,63 @@
 { colors, ... }:
 
 ''
-#!/usr/bin/env bash
+#! /usr/bin/env fish
 
-fork='-f'
-startup=false
-background="$HOME/.lock_screen.png"
-
-if pgrep -x swaylock >/dev/null
-then
+if pgrep -x swaylock > /dev/null
     echo "swaylock is already running"
     exit
+end
+
+# parse args
+argparse "n/no-fork" "s/startup" -- $argv; or exit $status
+
+set fork_arg
+if set -q $_flag_no_fork
+    echo "swaylock won't fork"
+    set fork_arg ""
 else
-    # parse args
-    while (( "$#" )); do
-        case "$1" in
-            -n|--no-fork)
-                echo "swaylock won't fork"
-                fork=""
-                shift
-                ;;
-            -s|--startup)
-                fork=""
-                startup=true
-                shift
-                echo
-        esac
-    done
-fi
+    set fork_arg '-f'
+end
 
-if [ "$startup" = false ] ; then
-    paplay "/usr/share/sounds/musicaflight/stereo/Lock.oga" &
-fi
+set background "$HOME/.lock_screen.jpg"
 
-primary="${colors.base05}c0"
-primaryFaded="${colors.base05}20"
-secondary="${colors.palette.primary}20"
-transparent="00000000"
-orange="${colors.palette.warning}"
+if ! set -q _flag_startup
+    pw-play "/usr/share/sounds/musicaflight/stereo/Lock.oga" &
+else
+    echo "running in startup mode"
+end
 
-swaylock $fork -i "$background" \
-    -e \
-    -t \
+set background "${colors.palette.background}"
+set primary "${colors.base05}c0"
+set primary_faded "${colors.base05}80"
+set secondary "${colors.palette.primary}80"
+set transparent "00000000"
+set warning "${colors.palette.warning}"
+
+# take screenshot and blur it
+grim $background
+convert $background \
+    -resize 5% \
+    -fill $background \
+    -colorize 25% \
+    -blur 10x2 \
+    -resize 2000% \
+    $background
+
+swaylock $fork_arg \
+    --image "$background" \
+\
+    --ignore-empty-password \
     --scaling=fill \
-    -L \
-    -l \
-    -c 000000 \
+    --disable-caps-lock-text \
+    --indicator-caps-lock \
+    --color 000000 \
 \
     --ring-color=$transparent \
     --ring-wrong-color=${colors.palette.alert}aa \
     --ring-ver-color=$transparent \
     --ring-caps-lock-color=$transparent \
-    --ring-clear-color=$orange \
+    --ring-clear-color=$warning \
 \
     --indicator-radius 128 \
     --indicator-thickness 4 \
@@ -62,9 +68,9 @@ swaylock $fork -i "$background" \
     --inside-clear-color=$transparent \
     --inside-caps-lock-color=$transparent \
 \
-    --key-hl-color=$primaryFaded \
+    --key-hl-color=$primary_faded \
     --bs-hl-color=$secondary \
-    --caps-lock-key-hl-color=$orange \
+    --caps-lock-key-hl-color=$warning \
     --caps-lock-bs-hl-color=$secondary \
     --separator-color=$transparent \
     -n \
@@ -73,12 +79,12 @@ swaylock $fork -i "$background" \
     --text-ver-color=$transparent \
     --text-clear-color=$transparent \
     --text-wrong-color=$transparent \
-    --text-caps-lock-color=$orange \
+    --text-caps-lock-color=$warning \
 \
     --font "sans Thin" \
     --font-size 128 \
 
-if [ "$startup" = true ] ; then
-    pw-play "$HOME/Music/MuseSounds/stereo/Hello.oga" &
-fi
+if set -q _flag_startup
+    pw-play "$HOME/Music/MuseSounds/stereo/Hello.oga"
+end
 ''
